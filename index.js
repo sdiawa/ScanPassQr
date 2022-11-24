c66d031dd580f23c9ba0950e446b97d2a0f87f15
sdiawa <sekoukaderisc@yahoo.fr>
2022-11-24 23:01:44 +0100

    config express

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3001;
 const cors = require("cors");
 const route = require("./router/ExpressRouter");
 app.use(cors({origin: '*'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(route);
-const http = require('http').createServer(app);
-http.listen(app.listen(port, function () {
+app.listen(port, function () {
     console.log("serveur ok sur le port " + port)
-}));
+});
